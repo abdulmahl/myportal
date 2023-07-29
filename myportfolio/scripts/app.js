52b8131822f514d911fedc2298c81d771f6d3913
abdulmahl <mahalanguma87@gmail.com>
2023-07-29 19:05:21 +0200

    .

--- myportfolio/scripts/app.js
+++ myportfolio/scripts/app.js
@@ -43,3 +43,16 @@ let toggleMenu = function() {
 
 const x = document.querySelector('#hamBtn');
 x.onclick = toggleMenu;
+
+
+const apiKey = 'cb266327ed92e9d613d6ebe5e148d6ca';
+const city = 'Johannesburg';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?'
+    + 'lat=-26.195246&lon=28.034088=' + city + '&units=metric&appid=' 
+    + apiKey;
+
+fetch(apiKey)
+    .then(response => response.json)
+    .then(jsonData => {
+        console.log(jsonData);
+    })
