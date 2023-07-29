4c4178c744deab8ec99b9130ec93eebfaa859aee
abdulmahl <mahalanguma87@gmail.com>
2023-07-29 18:21:06 +0200

    .

--- myportfolio/scripts/app.js
+++ myportfolio/scripts/app.js
@@ -1,28 +1,29 @@
+// ? Add days and months to display on the web page.
 let date = new Date();
 
 let daysOfWeek = [
-    "Sun",
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat"
+    'Sun',
+    'Mon',
+    'Tue',
+    'Wed',
+    'Thu',
+    'Fri',
+    'Sat'
 ];
 
 let months = [
     'Jan',
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec'
 ];
 
 let day = daysOfWeek[date.getDay()];
@@ -33,4 +34,12 @@ const currentYear = document.querySelector('#year');
 const dateString = document.querySelector('#dateString');
 
 currentYear.textContent = year;
-dateString.textContent = `${day} ${date.getDate()} ${month}, ${year} `
+dateString.textContent = `${day} ${date.getDate()} ${month}, ${year}`;
+
+let toggleMenu = function() {
+    document.querySelector('#primaryNav').classList.toggle('open');
+    document.querySelector('#hamBtn').classList.toggle('open');
+} 
+
+const x = document.querySelector('#hamBtn');
+x.onclick = toggleMenu;
